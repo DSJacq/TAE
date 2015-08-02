5af86a2df59dbfe9e96781daafd0e832b5350112
Jacque Carvalho <ds.jacque@gmail.com>
2015-08-02 12:24:26 -0300

    Update and rename ClimateChange to ClimateChange.R

--- W02-LinearRegression/ClimateChange.R
+++ W02-LinearRegression/ClimateChange.R
@@ -1,6 +1,10 @@
 ### TAE H02 - CLIMATE CHANGE
 
-#Source: http://www.cru.uea.ac.uk/cru/data/temperature/
+#Source: Climatic Research Unit
+#URL: http://www.cru.uea.ac.uk/cru/data/temperature/
+
+
+
 
 # 1.1 CREATING OUR FIRST MODEL  
 climate = read.csv("climate_change.csv")
@@ -11,16 +15,28 @@ test = subset(climate, Year > 2006)
 climateLM = lm(Temp ~ MEI + CO2 + CH4 + N2O + CFC.11 + CFC.12 + TSI + Aerosols, data = train)
 summary(climateLM)
 
+
+
+
 # 2.2 UNDERSTANDING THE MODEL
 cor(train)
+
+
+
 
 # 3.0 SIMPLIFYING THE MODEL
 climateLM2 = lm(Temp ~ MEI + N2O + TSI + Aerosols, data = train)
 summary(climateLM2)
 
+
+
+
 # 4.0 AUTOMATICALLY BUILDING THE MODEL
 climateStep = step(climateLM)
 summary(StepModel)
+
+
+
 
 # 5.0 TESTING ON UNSEEN DATA  
 tempPredict = predict(climateStep, newdata = test)
